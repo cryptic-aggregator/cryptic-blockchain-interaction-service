8f1a83102a61506396cce84f1b22dade58d0aede
Stanislav Molchan <stasek.molchan@gmail.com>
2025-03-01 21:31:42 +0200

    fix

--- src/database/redis/redisClient.ts
+++ src/database/redis/redisClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 
 const redisClient = createClient({
-    url: 'redis://localhost:6379'
+    url: 'redis://20.215.241.137:6379'
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
